9b60b0ef206b41910149a6a0a0c625baad527bb9
blizniukp <blizniukp@gmail.com>
2022-02-06 00:15:27 +0100

    feat: Support Roundpad device

--- data/run.py
+++ data/run.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
-import getopt
 import json
 import logging
 import paho.mqtt.client as mqtt
 import sys
-import time
+
 
 CONFIG_PATH = "/data/options.json"
 CONFIG = None
 DEV_LIST = None
+SWITCH_LIST = None
+MAX_BUTTON_PER_DEV = 6
+MAX_BUTTON_FUNCTIONS = 2
+
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(funcName)s() - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -38,53 +41,61 @@ def get_dev_list(conf):
 	return dlist
 
 
-def create_switches(mqttc, conf):
-	devices = conf["devices"]
-	for device in devices:
-		logger.info("Create new switch: {}".format((str(device))))
-		topic = "homeassistant/switch/{}/config".format(device['device_id'])
+def genearte_switch_list(device_list):
+	slist = []
+	for device in device_list:
+		device_id = device["address"].replace(':', '_')
+		for button_no in range(1, MAX_BUTTON_PER_DEV + 1):
+			for func_no in range(1, MAX_BUTTON_FUNCTIONS + 1):
+				b = {
+					'dev_address': device_id,
+					'name': device['name'] + '_B' + str(button_no) + '_F' + str(func_no),
+					'unique_id': device_id + '_B' + str(button_no) + '_F' + str(func_no),
+					'value_on': func_no,
+					'value_off': 0,
+				}
+				slist.append(b)
+	return slist
+
+
+def create_switches(mqttc, switches):
+	for switch in switches:
+		logger.info("Create new switch: {}".format((str(switch))))
+		topic = "homeassistant/switch/{}/config".format(switch['unique_id'])
 		payload = {
-			"name": device["name"],
-			"unique_id": device['device_id'],
-			"command_topic": "homeassistant/switch/{}/set".format(device['device_id']),
-			"state_topic": "homeassistant/switch/{}/state".format(device['device_id'])
+			"name": switch["name"],
+			"unique_id": switch['unique_id'],
+			"command_topic": "homeassistant/switch/{}/set".format(switch['unique_id']),
+			"state_topic": "homeassistant/switch/{}/state".format(switch['unique_id'])
 		}
 		pub_message(mqttc, topic, json.dumps(payload))
 
 
-def create_triggers(mqttc, conf):
-	devices = conf["devices"]
-	for device in devices:
-		logger.info("Create new switch: {}".format((str(device))))
-		topic = "homeassistant/device_automation/{}/config".format(device['device_id'])
-		payload = {
-			"automation_type": "trigger",
-			"topic": "",
-			"type": "button_short_press",
-			"subtype": "button_1",
-			"device": {
-				"name": device["name"],
-				"identifiers": [device['device_id']]
-			}
-		}
-		pub_message(mqttc, topic, json.dumps(payload))
-
-
-def pub_switch_status(mqttc, device, value):
-	topic = "homeassistant/switch/{}/state".format(device['device_id'])
-	payload = "ON" if int(value) == 1 else "OFF"
+def pub_switch_status(mqttc, switch, func_no):
+	topic = "homeassistant/switch/{}/state".format(switch['unique_id'])
+	payload = "OFF" if func_no == 0 else "ON"
 	pub_message(mqttc, topic, str(payload))
 
 
-def pub_value(mqttc, payload):
-	"""b'{"address": "F1:34:D5:DF:B8:3B", "is_notify": True, "val_len": 1, "val": 00}'"""
-	global CONFIG
+def parse_and_pub_value(mqttc, payload):
+	global SWITCH_LIST
 	vinfo = json.loads(payload.decode('utf-8'))
-	devices = CONFIG["devices"]
-	for device in devices:
-		if device["address"] == vinfo["address"]:
-			pub_switch_status(mqttc, device, vinfo["val"])
-			return
+	device_id = vinfo["address"].replace(':', '_')
+	button_no = int(vinfo["val"][:2])
+	func_no = int(vinfo["val"][2:4])
+	unique_id = device_id + '_B' + str(button_no) + '_F' + str(func_no)
+	if func_no > 0:
+		for switch in SWITCH_LIST:
+			if str(switch["unique_id"]) == str(unique_id):
+				logger.info("Publish switch status. Switch: {}, value: {}".format(str(switch), str(func_no)))
+				pub_switch_status(mqttc, switch, func_no)
+				return
+	elif func_no == 0:
+		unique_id = device_id + '_B' + str(button_no) + '_F'
+		for switch in SWITCH_LIST:
+			if str(switch["unique_id"]).startswith(unique_id):
+				logger.info("Publish switch status. Switch: {}, value: {}".format(str(switch), str(func_no)))
+				pub_switch_status(mqttc, switch, func_no)
 
 
 def pub_message(mqttc, topic, payload):
@@ -93,61 +104,66 @@ def pub_message(mqttc, topic, payload):
 
 
 def on_connect(mqttc, obj, flags, rc):
-	logging.info("rc: " + str(rc))
+	logger.info("rc: " + str(rc))
 
 
 def on_message(mqttc, obj, msg):
-	logging.debug(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+	logger.debug(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 	top = msg.topic.split('/')
-	logging.info("Command: {}".format(top[3]))
+	logger.info("Command: {}".format(top[3]))
 	if top[3] == 'getDevList':
 		resp = json.dumps(DEV_LIST)
 		pub_message(mqttc, "/ble2mqtt/dev/devList", resp)
 	if top[3] == 'value':
-		logging.info("Receive value from device: {}".format(str(msg.payload)))
-		pub_value(mqttc, msg.payload)
+		logger.info("Receive value from device: {}".format(str(msg.payload)))
+		parse_and_pub_value(mqttc, msg.payload)
 
 
 def on_publish(mqttc, obj, mid):
-	logging.info("mid: " + str(mid))
+	logger.info("mid: " + str(mid))
 
 
 def on_subscribe(mqttc, obj, mid, granted_qos):
-	logging.info("Subscribed: " + str(mid) + " " + str(granted_qos))
+	logger.info("Subscribed: " + str(mid) + " " + str(granted_qos))
 
 
 def on_log(mqttc, obj, level, string):
-	logging.info(string)
+	logger.info(string)
 
 
 def main(argv):
 	global DEV_LIST
 	global CONFIG
+	global SWITCH_LIST
 	logger.info("Start python script")
 
-	logger.info("Load configuration")
-	CONFIG = load_config(CONFIG_PATH)
-	DEV_LIST = get_dev_list(CONFIG)
+	try:
+		logger.info("Load configuration")
+		CONFIG = load_config(CONFIG_PATH)
+		DEV_LIST = get_dev_list(CONFIG)
+		SWITCH_LIST = genearte_switch_list(DEV_LIST)
 
-	logger.info("Config: {}".format(str(CONFIG)))
-	logger.info("Device list: {}".format(str(DEV_LIST)))
+		logger.info("Config: {}".format(str(CONFIG)))
+		logger.info("Device list: {}".format(str(DEV_LIST)))
+		logger.info("Switch list: {}".format(str(SWITCH_LIST)))
 
-	mqttc = mqtt.Client("Ble2Mqtt", clean_session=True)
-	mqttc.on_message = on_message
-	mqttc.on_connect = on_connect
-	mqttc.on_publish = on_publish
-	mqttc.on_subscribe = on_subscribe
+		mqttc = mqtt.Client("Ble2Mqtt", clean_session=True)
+		mqttc.on_message = on_message
+		mqttc.on_connect = on_connect
+		mqttc.on_publish = on_publish
+		mqttc.on_subscribe = on_subscribe
 
-	mqttc.username_pw_set(username=CONFIG["username"], password=CONFIG["password"])
-	mqttc.connect(CONFIG['address'], int(CONFIG['port']), 60)
-	logger.info("Connected to MQTT broker: {}".format(CONFIG['address']))
+		mqttc.username_pw_set(username=CONFIG["username"], password=CONFIG["password"])
+		mqttc.connect(CONFIG['address'], int(CONFIG['port']), 60)
+		logger.info("Connected to MQTT broker: {}".format(CONFIG['address']))
 
-	mqttc.subscribe(CONFIG['sub_topic'], CONFIG['qos'])
+		mqttc.subscribe(CONFIG['sub_topic'], CONFIG['qos'])
 
-	create_switches(mqttc, CONFIG)
-	#create_triggers(mqttc, CONFIG)
-	mqttc.loop_forever()
-	logger.info("Stop python script")
+		create_switches(mqttc, SWITCH_LIST)
+		mqttc.loop_forever()
+		logger.info("Stop python script")
+	except Exception as ex:
+		logger.exception(ex)
 
 
 if __name__ == "__main__":
